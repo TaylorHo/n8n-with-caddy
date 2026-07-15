2d2de08bdfa4fa173e430665ad9b255245bc1936
Taylor Hoffmann <taylor@hoffmann.io>
2026-07-15 15:11:00 -0300

    refactor: upgraded n8n to v2 + adjusted momery usage limits

--- init.sh
+++ init.sh
@@ -3,10 +3,23 @@
 # Execute commands of different lines one by one
 
 # The value below can be "debian" or "ubuntu"
-LINUX_DISTRO=debian
+LINUX_DISTRO=ubuntu
 
 sudo apt update && sudo apt install ca-certificates curl git ufw -y && sudo install -m 0755 -d /etc/apt/keyrings && sudo curl -fsSL https://download.docker.com/linux/$LINUX_DISTRO/gpg -o /etc/apt/keyrings/docker.asc && sudo chmod a+r /etc/apt/keyrings/docker.asc && sudo chmod a+r /etc/apt/keyrings/docker.asc && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/$LINUX_DISTRO $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && sudo apt update && sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y && sudo usermod -aG docker $USER
 
 newgrp docker
 
 cd ~ && git clone https://github.com/TaylorHo/n8n-with-caddy.git n8n && cd n8n && docker volume create caddy_data && docker volume create caddy_config && docker volume create n8n_data && echo "Edit the informations in .env and in Caddifile, then run 'docker-compose up -d' command"
+
+# Add swap memory:
+sudo fallocate -l 512M /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo tee -a /etc/sysctl.conf <<EOF
+vm.swappiness=10
+vm.vfs_cache_pressure=50
+EOF
+sudo sysctl -p
+sudo systemctl disable --now systemd-oomd
